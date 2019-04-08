dc8e7ac2bee5895f34f1da2cc3384a2d9ce53e57
evan999 <emeshberge@gmail.com>
2019-04-07 23:33:06 -0400

    Base constructors with inheritance

--- ConstructorsInheritance/Program.cs
+++ ConstructorsInheritance/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace ConstructorsInheritance
 {
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized.");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine("Vehicle is being initialized. {0}", registrationNumber);
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized. {0}", registrationNumber);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var car = new Car("XYZ1234");
+            Console.ReadLine();
+
         }
     }
 }
